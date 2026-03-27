2cd174e17b1af042b8dadc454b201610dc89a09a
davidfreire-lgtm <david.freire@escola.pr.gov.br>
2026-03-26 21:23:15 -0300

    Add files via upload

---
+++ SQLite.15.sql
@@ -0,0 +1,9 @@
+insert INTO sala (matricula, nome, idade, turma)
+VALUES
+(1, 'ronado', 10, 1),
+(2, 'davu', 11, 2),
+(3, 'doc0', 13, 4),
+(4, 'alex', 18, 10),
+( 5, 'joco', 11, 11);
+
+SELECT * FROM sala;
---
+++ SQLite.sql
@@ -0,0 +1,9 @@
+CREATE TABLE sala(
+ id integer PRIMARY key AUTOINCREMENT,
+  matricula integer,
+  nome text,
+  idade integer,
+  turma text
+  );
+  
+  SELECT * FROM sala;
